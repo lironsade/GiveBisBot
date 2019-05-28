27ff46ce104b82c1105674919e19e381951d825d
Arikshtine <arik.shtine@gmail.com>
2019-05-28 13:31:26 +0300

    update subscribers_db

--- give-bis-bot/subscribers_db.py
+++ give-bis-bot/subscribers_db.py
@@ -9,19 +9,20 @@ class SubscribersDatabase:
         self.create_table()
 
     def create_table(self):
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS subscribers
-                               (chat_id type UNIQUE)''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Restaurants
+                               (chat_id TEXT UNIQUE, rest_name TEXT, food TEXT, 
+                               tel TEXT, location TEXT, rest_id INTEGER PRIMARY KEY(rest_id))''')
 
-    def insert(self, chat_id):
-        self.cursor.execute('''INSERT INTO subscribers VALUES ({})'''.format(chat_id))
+    def insert(self, chat_id, rest_name, food, tel, location, rest_id):
+        self.cursor.execute('''INSERT INTO Restaurants VALUES ({})'''.format(chat_id, rest_name, food, tel, location, rest_id))
         self.conn.commit()
 
-    def delete(self, chat_id):
-        self.cursor.execute('''DELETE FROM subscribers WHERE chat_id={}'''.format(chat_id))
+    def delete(self, rest_id):
+        self.cursor.execute('''DELETE FROM subscribers WHERE id={}'''.format(rest_id))
         self.conn.commit()
 
     def close(self):
         self.conn.close()
 
-    def get_all_subscribers(self):
-        return [chat_id for (chat_id, ) in self.cursor.execute('SELECT chat_id FROM subscribers')]
+    def get_all_restaurants(self):
+        return [rest_id for (rest_id, ) in self.cursor.execute('SELECT rest_id FROM Restaurants')]
