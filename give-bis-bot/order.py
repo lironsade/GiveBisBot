2b22f73344de44077c64b670fac2059ff259e3eb
Avichail Vogel <avichail@cs.huji.ac.il>
2019-06-16 16:17:47 +0300

    allow multiple orders for one user

--- give-bis-bot/order.py
+++ give-bis-bot/order.py
@@ -4,33 +4,40 @@ DEFAULT_PRICE = 50
 
 
 class Order:
-    class FoodType:
-        def __init__(self, name, price, notes=None):
-            self.type = FoodItem(name, price)
-            self.notes = notes
+    class FoodTypeOrder:
+        notes = []
+
+        def __init__(self, item, notes=None):
+            assert isinstance(item, FoodItem)
+            self.item = item
+            self.notes.append(notes)
 
         def add_note(self, note):
-            self.notes += note
+            self.notes.append(note)
 
         def delete_note(self, note):
-            self.notes = self.notes.replace(note, "")
+            self.notes.remove(note)
 
         def __str__(self):
-            return f"{self.type.__str__()}  \n Your notes:  {self.notes}"
+            return f"{self.item.__str__()}  \n Your notes:  {self.notes} \n"
 
-    _food_type = [FoodType('Falafel',DEFAULT_PRICE), FoodType('Pizza',DEFAULT_PRICE)]
+    my_orders = []
 
-    def __init__(self, food_type, food_note, name, location, payment):
-        self._food_note = food_note
+    def __init__(self, name, location, payment):
         self._name = name
         self._location = location
         self._payment = payment
 
-    def add_food_type(self, new_food_type):
-        self._food_type.append(new_food_type)
-
-    def get_status(self):
-        pass
+    def place(self, food_type):
+        """
+        place new order
+        :type food_type: FoodType
+        """
+        assert isinstance(food_type, Order.FoodTypeOrder)
+        self.my_orders.append(food_type)
 
     def __str__(self):
-        pass
+        return f"{food_order.__str__() for food_order in self.my_orders}"
+
+    def get_status(self):
+        return f"Name: {self._name} \n Location: {self._location} \n Payment: {self._payment}\n Order: {self.__str__()}"
