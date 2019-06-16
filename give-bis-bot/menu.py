37aa75e46a54c10b8ad5669d94bea25964567227
Liron Sade <liron@cs.huji.ac.il>
2019-06-16 16:27:33 +0300

    improved menu api

--- give-bis-bot/menu.py
+++ give-bis-bot/menu.py
@@ -2,12 +2,18 @@ from fooditem import FoodItem
 
 class Menu:
     
-    def __init__(self, items=[]):
-        self.items = items
+    def __init__(self, items=None):
+        if items:
+            self.items = dict()
+        else:
+            self.items = items
 
     def AddItem(self, fooditem):
         self.items.append(fooditem)
 
     def RemoveItem(self, fooditem):
         self.items.remove(fooditem)
+
+    def GetItem(self, name):
+        return self.items.get(name, None)
 
