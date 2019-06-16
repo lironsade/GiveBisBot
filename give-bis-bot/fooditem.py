16ff4ef358125213ca6dc27128132c7d794e80f7
Liron Sade <liron@cs.huji.ac.il>
2019-06-16 12:52:21 +0300

    fooditem str

--- give-bis-bot/fooditem.py
+++ give-bis-bot/fooditem.py
@@ -2,3 +2,6 @@ class foodItem:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+    def __str__(self):
+        return f"{self.name} costs {self.price}"
