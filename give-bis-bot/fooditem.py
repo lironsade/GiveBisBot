4e98424c98fb817f0aafde3b7a9f6d83da846151
Arie Shtine <arie.shtine@cs.huji.ac.il>
2019-06-25 15:32:32 +0300

    Revert "Update fooditem.py"

--- give-bis-bot/fooditem.py
+++ give-bis-bot/fooditem.py
@@ -1,11 +1,7 @@
 class FoodItem:
-    def __init__(self, name, price, comment=None):
+    def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.comment = comment
 
     def __str__(self):
-    if self.comment == None
         return f"{self.name} costs {self.price}"
-    else:
-        return f"{self.name} costs {self.price}: {self.comment}"
