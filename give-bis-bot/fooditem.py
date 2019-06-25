d096d598456914a60183e1cbb072b8108630862e
Tal Avni <talavni11@cs.huji.ac.il>
2019-06-25 14:31:02 +0300

    Update fooditem.py
    added option to add comment about fooditem

--- give-bis-bot/fooditem.py
+++ give-bis-bot/fooditem.py
@@ -1,7 +1,11 @@
 class FoodItem:
-    def __init__(self, name, price):
+    def __init__(self, name, price, comment=None):
         self.name = name
         self.price = price
+        self.comment = comment
 
     def __str__(self):
+    if self.comment == None
         return f"{self.name} costs {self.price}"
+    else:
+        return f"{self.name} costs {self.price}: {self.comment}"
